e93e2932c7a88eeb0967e3c161925f16fb17ebd5
Vittorio Sozzi <vittorio.sozzi@elfo.net>
2019-12-05 17:47:05 +0100

    Forms

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,11 @@ const routes = [{
     path: '/dashboard',
     name: 'dashboard',
     component: () => import('../views/Dashboard.vue')
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () => import('../views/Signup.vue')
   }
 ]
 
